efdc4425b0b590e8b5148e83689fd70c57cbe270
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-04-16 18:40:28 +0000

    added base for request.rb

--- lib/coc-permitzones/connection.rb
+++ lib/coc-permitzones/connection.rb
@@ -3,11 +3,13 @@ require 'soda/client'
 class Connection
   client = SODA::Client.new({:domain => "data.cityofchicago.org", :app_token => ENV["APP_TOKEN"]})
 
-  results = client.get("ys7w-i4tk", :$limit => 5000)
-
-  puts "Got #{results.count} results. Dumping first results:"
-  results.first.each do |k, v|
-    puts "#{key}: #{value}"
+  def self.api
+    results = client.get("ys7w-i4tk")
   end
+
+  # puts "Got #{results.count} results. Dumping first results:"
+  # results.first.each do |k, v|
+  #   puts "#{key}: #{value}"
+  # end
 
 end
