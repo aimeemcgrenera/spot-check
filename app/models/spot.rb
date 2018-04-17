84297698c8c841d6c1fe208ee0dd7394f55a662c
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-04-17 15:09:29 +0000

    added validations and serialization attributes for spots

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,8 @@
 class Spot < ApplicationRecord
+  validates :address_number, :street_direction, :street_name, :street_type, presence: true
+  validates :street_direction, inclusion: { in: %w(N S E W),
+    message: "%{value} is not a valid direction, please enter N S E or W" }
+
+    #street types: AVE, BLVD, ST, PL, TER, RD, PKWY, DR, CT, LN, WAY
+
 end
--- app/serializers/spot_serializer.rb
+++ app/serializers/spot_serializer.rb
@@ -1,3 +1,3 @@
 class SpotSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :address_number, :street_direction, :street_name, :street_type
 end
