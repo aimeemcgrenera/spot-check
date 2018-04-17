12266670f35a834711eebda65c460866b30b612d
Aimee McGrenera <aimee.mcgrenera@gmail.com>
2018-04-17 14:34:27 +0000

    added spots resource and migrations

---
+++ db/migrate/20180417142340_create_spots.rb
@@ -0,0 +1,12 @@
+class CreateSpots < ActiveRecord::Migration[5.1]
+  def change
+    create_table :spots do |t|
+      t.integer :address_number
+      t.integer :street_direction
+      t.string :street_name
+      t.string :street_type
+
+      t.timestamps
+    end
+  end
+end
