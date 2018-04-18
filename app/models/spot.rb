24d522c572598cecf07826964962fba798fd5328
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-04-17 21:09:02 -0500

    added upcase for street name in class

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,8 +1,13 @@
-class Spot < ApplicationRecord
+class Spot < ActiveRecord::Base
   validates :address_number, :street_direction, :street_name, :street_type, presence: true
   validates :street_direction, inclusion: { in: %w(N S E W),
     message: "%{value} is not a valid direction, please enter N S E or W" }
 
+    before_save :uppercase
     #street types: AVE, BLVD, ST, PL, TER, RD, PKWY, DR, CT, LN, WAY
 
+    def uppercase
+      street_name.upcase!
+      street_direction.upcase!
+    end
 end
--- app/models/zone.rb
+++ app/models/zone.rb
@@ -1,10 +1,12 @@
 require 'soda/client'
 
-class Zone < ApplicationRecord
+class Zone < ActiveRecord::Base
 
+  #get data based on street name
   def self.getData(street_name)
     client = SODA::Client.new({:domain => "data.cityofchicago.org", :app_token => ENV["APP_TOKEN"]})
     results = client.get("ys7w-i4tk", :street_name => street_name)
   end
+
 
 end
