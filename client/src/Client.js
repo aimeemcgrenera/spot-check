a803116a8a1dbeefaffa2cb27ed3510694a31052
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-08 21:04:54 -0500

    added Client.js skeleton

---
+++ client/src/Client.js
@@ -0,0 +1,16 @@
+import fetch from 'isomorphic-fetch';
+
+function search(query) {
+
+}
+
+function checkStatus(response) {
+
+}
+
+function parseJSON(response) {
+
+}
+
+const Client = { search };
+export default Client;
