7ac687cef95f731589e6be24dbedd070c4a16420
Aimee Carney <aimeecarney@Aimees-MacBook-Pro.local>
2018-05-08 21:02:53 -0500

    added app.js render

--- app/controllers/zones_controller.rb
+++ app/controllers/zones_controller.rb
@@ -6,7 +6,7 @@ class ZonesController < ApplicationController
     street_name = params[:street_name]
     street_type = params[:street_type]
 
-    results = Zone.getData("E", "W", "ROSCOE", "ST")
+    results = Zone.getData(odd_even, street_direction, street_name, street_type)
 
     render :json => results
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ZoneSearch from './ZoneSearch';
+// import ZoneList from './ZoneList';
 
 class App extends Component {
+
   render() {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <div className='ui text container'>
+          <ZoneSearch
+          />
+
+        </div>
       </div>
     );
   }
